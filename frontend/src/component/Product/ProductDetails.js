bf06e49b7d062ddb94ac260c9d2cc442bf541035
Mohak Ahuja <mohakaahuaj@outlook.com>
2023-04-02 21:27:03 +0530

    new build.

--- frontend/src/component/Product/ProductDetails.js
+++ frontend/src/component/Product/ProductDetails.js
@@ -201,7 +201,7 @@ const ProductDetails = () => {
                   <>
                     <h3>
                       Similar products:{" "}
-                      <span style={{ fontWeight: "normal", backgroundColor: "whitesmoke", padding: "0.2rem" }}>
+                      <span style={{ fontWeight: "normal", backgroundColor: "whitesmoke", paddingLeft: "0.2rem", fontSize:"15px" }}>
                         {product.category}
                       </span>
                     </h3>
