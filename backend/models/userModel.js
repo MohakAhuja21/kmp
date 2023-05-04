f5253cbbf2edbff0a8a329ac5a91ebf27f4a2fff
Mohak Ahuja <mohakaahuaj@outlook.com>
2023-05-04 16:01:02 +0530

    JWT_EXPIRE

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -53,7 +53,7 @@ userSchema.pre("save", async function (next) {
 // JWT tOKEN
 userSchema.methods.getJWTToken = function () {
   return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRE,
+    expiresIn: "1d",
   });
 };
 
