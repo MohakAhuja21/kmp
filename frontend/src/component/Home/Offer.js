53b7c6700a14ae1fbaeb0c99e9b0bfaa1b589b5b
Mohak Ahuja <mohakaahuaj@outlook.com>
2023-06-22 13:16:37 +0530

    new build and css fixes

--- frontend/src/component/Home/Offer.js
+++ frontend/src/component/Home/Offer.js
@@ -1,69 +1,86 @@
-import React, { useEffect, useState } from 'react';
-import './Offer.css';
+import React, { useEffect, useState } from "react";
+import "./Offer.css";
 import { Link } from "react-router-dom";
 
 const Offer = () => {
   const testimonials = [
     {
-      name: 'ELECTRAL POWDER',
-      scheme: '28+2',
-      image: 'https://newassets.apollo247.com/pub/media/catalog/product/e/l/ele0012.jpg',
-      productId: '644e006eb9e1261b11398bee', // Add productId for linking
-      // testimonial:
-      //   'Neque volutpat ac tincidunt vitae semper quis lectus nulla at volutpat diam ut venenatis tellus in metus vulputate eu scelerisque felis imperdiet proin fermentum leo vel orci porta non pulvinar neque laoreet suspendisse interdum consectetur',
+      name: "ELECTRAL POWDER",
+      scheme: "28+2",
+      image:
+        "https://newassets.apollo247.com/pub/media/catalog/product/e/l/ele0012.jpg",
+      productId: "644e006eb9e1261b11398bee", // Add productId for linking
     },
     {
-      name: 'ELECTRAL LIQUID',
-      scheme: '5+1',
-      image: 'https://www.electral.co.in/images/products/tetrapakimg.png',
-      productId:'644e0165b9e1261b11398df5',
-      // testimonial:
-      //   'Elementum tempus egestas sed sed risus pretium quam vulputate dignissim suspendisse in est ante in nibh mauris cursus mattis molestie a iaculis at erat pellentesque adipiscing commodo elit at imperdiet dui accumsan sit amet nulla',
+      name: "ELECTRAL LIQUID",
+      scheme: "5+1",
+      image: "https://www.electral.co.in/images/products/tetrapakimg.png",
+      productId: "644e0165b9e1261b11398df5",
     },
     {
-      name: 'QUADRIDERM RF CREAM',
-      scheme: '4+1',
-      image: 'https://res.fkhealthplus.com/incom/images/product/Quadriderm-RF-1669985958-10000666-1.jpg',
-      productId:'6479cbf53fdabf22e0e7013b',
-      // testimonial:
-      //   'Orci nulla pellentesque dignissim enim sit amet venenatis urna cursus eget nunc scelerisque viverra mauris in aliquam sem fringilla ut morbi tincidunt augue interdum velit euismod in pellentesque massa placerat duis ultricies lacus sed turpis',
+      name: "QUADRIDERM RF CREAM",
+      scheme: "5+1",
+      image:
+        "https://res.fkhealthplus.com/incom/images/product/Quadriderm-RF-1669985958-10000666-1.jpg",
+      productId: "6479cbf53fdabf22e0e7013b",
     },
   ];
 
   const [currentSlide, setCurrentSlide] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
 
   const nextSlide = () => {
     setCurrentSlide((prevSlide) => (prevSlide + 1) % testimonials.length);
   };
 
   const prevSlide = () => {
-    setCurrentSlide((prevSlide) =>
-      (prevSlide + testimonials.length - 1) % testimonials.length
+    setCurrentSlide(
+      (prevSlide) => (prevSlide + testimonials.length - 1) % testimonials.length
     );
   };
 
   useEffect(() => {
-    // This code will be executed when the component is mounted
-    document.getElementById('loader').style.display = 'none';
-    document.getElementById('box').style.display = 'block';
+    const loader = document.getElementById("loader");
+    const box = document.getElementById("box");
+    loader.style.display = "none";
+    box.style.display = "block";
   }, []);
+
+  const { name, scheme, image, productId } = testimonials[currentSlide];
 
   return (
     <div className="OfferCon">
       <div id="loader"></div>
       <div id="box">
         <div className="wrapper">
-          <div className="testimonial-container" id="testimonial-container">
-            <div id="testimonial1" className="active">
-        <h1 className='offerCon_head'>Ongoing Offers</h1>
-        <Link style={{textDecoration:"none"}} to={`/product/${testimonials[currentSlide].productId}`}>
-              <img
-                src={testimonials[currentSlide].image}
-                alt="Testimonial"
-              />
-              <h3>{testimonials[currentSlide].name}</h3>
-              <p>scheme: <span>{testimonials[currentSlide].scheme}</span></p>
-              {/* <p>{testimonials[currentSlide].testimonial}</p> */}
+          <div className={`testimonial-container`} id="testimonial-container">
+            <div
+              className={`testimonial1 active ${isLoading ? "loading" : ""}`}
+            >
+              <h1 className="offerCon_head">Ongoing scheme</h1>
+              <Link
+                style={{ textDecoration: "none" }}
+                to={`/product/${productId}`}
+              >
+                <div className="image-wrapper">
+                  <img
+                    src={image}
+                    alt="Testimonial"
+                    onLoad={() => setIsLoading(false)}
+                    className={isLoading ? "hidden" : "visible"}
+                  />
+                  {isLoading && (
+                    <div className="loading-overlay">Loading...</div>
+                  )}
+                </div>
+                {!isLoading && (
+                  <>
+                    <h3>{name}</h3>
+                    <p>
+                      scheme: <span>{scheme}</span>
+                    </p>
+                  </>
+                )}
               </Link>
             </div>
           </div>
--- frontend/src/component/Home/SliderHome.js
+++ frontend/src/component/Home/SliderHome.js
@@ -1,37 +1,34 @@
-import React from 'react'
-import "./Slider.css"
+import React from "react";
+import "./Slider.css";
 
 function Slider() {
+  const slides = [
+    "https://www.abbott.co.in/etc.clientlibs/abbott-platform/clientlibs/clientlib-site/resources/images/abbott-logo.png",
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/Cipla_logo.svg/2560px-Cipla_logo.svg.png",
+    "https://upload.wikimedia.org/wikipedia/commons/3/3f/Mankind_Serving_Life.png",
+    "https://ddnews.gov.in/sites/default/files/Zydus-Cadila.jpg",
+    "https://www.leefordonline.in/images/logo.png?v=1.2",
+    "https://drmorepen.com/wp-content/uploads/2021/02/logo.png",
+    "https://iphex-india.com/uploads/company_logo_2018/173_Knoll_Healthcare_Pvt._Ltd._comp_logo_20180405154647.jpg",
+    "https://static.wixstatic.com/media/1b134f_082f6de6917e431ab3915ee6a724fbd1~mv2.png/v1/fill/w_340,h_340,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Luen%20Cheong%20Hong%20LCH%20Logo.png",
+  ];
+
+  const slideDuration = slides.length * 6;
+
   return (
     <div className="sliderCon">
-  <div className="slide-track">
-    <div className="slide">
-      <img src="https://www.abbott.co.in/etc.clientlibs/abbott-platform/clientlibs/clientlib-site/resources/images/abbott-logo.png"  alt="abott" />
+      <div
+        className="slide-track"
+        style={{ animationDuration: `${slideDuration}s` }}
+      >
+        {slides.map((slide, index) => (
+          <div className="slide" key={index}>
+            <img src={slide} alt={`Slide ${index}`} />
+          </div>
+        ))}
+      </div>
     </div>
-    <div className="slide">
-      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/Cipla_logo.svg/2560px-Cipla_logo.svg.png"  alt="cipla" />
-    </div>
-    <div className="slide">
-      <img src="https://upload.wikimedia.org/wikipedia/commons/3/3f/Mankind_Serving_Life.png"  alt="mankind" />
-    </div>
-    <div className="slide">
-      <img src="https://ddnews.gov.in/sites/default/files/Zydus-Cadila.jpg"  alt="zydus cadilia" />
-    </div>
-    <div className="slide">
-      <img src="https://www.leefordonline.in/images/logo.png?v=1.2"  alt="leeford" />
-    </div>
-    <div className="slide">
-      <img src="https://drmorepen.com/wp-content/uploads/2021/02/logo.png"  alt="dr.morpen" />
-    </div>
-    <div className="slide">
-      <img src="https://iphex-india.com/uploads/company_logo_2018/173_Knoll_Healthcare_Pvt._Ltd._comp_logo_20180405154647.jpg"  alt="knoll" />
-    </div>
-    <div className="slide">
-      <img src="https://static.wixstatic.com/media/1b134f_082f6de6917e431ab3915ee6a724fbd1~mv2.png/v1/fill/w_340,h_340,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Luen%20Cheong%20Hong%20LCH%20Logo.png"  alt="wockhardt" />
-    </div>
-  </div>
-</div>
-  )
+  );
 }
 
-export default Slider
+export default Slider;
